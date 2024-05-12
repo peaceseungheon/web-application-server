6d05f475a3adc3073f7cd06712da935c2dd0550d
seungheon <seungheon@apexsoft.co.kr>
2024-05-12 21:53:29 +0900

    index.html 반환 하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,21 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
+import java.nio.file.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+    private static final String DEFAULT_PATH = "/index.html";
 
     private Socket connection;
 
@@ -24,13 +29,61 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            byte[] body = null;
+            String line;
+            do {
+                line = br.readLine();
+                if(line == null){
+                    throw new RuntimeException("올바른 HTTP 요청이 아닙니다.");
+                }
+                String[] tokens = line.split(" ");
+                if(this.isHttpMethodLine(tokens)){
+                    body = getResource(tokens[1]);
+                }
+            }
+            while(!line.isEmpty());
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            if(body != null){
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private byte[] getResource(String uri) throws IOException {
+        File file;
+        if(uri.equals("/")){
+            file = new File("./webapp" + DEFAULT_PATH);
+        }else {
+            file = new File("./webapp" + uri);
+        }
+        try {
+            return Files.readAllBytes(file.toPath());
+        }catch (IOException e){
+            throw new RuntimeException("파일을 읽지 못했습니다.");
+        }
+    }
+
+    private boolean isHttpMethodLine(String[] tokens){
+        if(tokens.length == 0){
+            return false;
+        }
+        String method = tokens[0];
+
+        String[] methods = new String[]{"GET", "POST", "PUT", "DELETE"};
+
+        boolean isHttpMethodLine = false;
+        for(String m : methods){
+            if(method.equals(m)){
+                isHttpMethodLine = true;
+                break;
+            }
+        }
+        return isHttpMethodLine;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
